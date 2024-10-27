242389fd7e8b8cf5c387b142d00ee175e77edd0f
Trikita73 <diachenkoandrii7@gmail.com>
2024-10-27 13:16:42 +0200

    arr

--- array.ts
+++ array.ts
@@ -1,5 +1,10 @@
 // списки
 const arr: string[] = ['a', 'b', 'c'];
+
+let el = arr[0]
+let el2 = arr.pop()
+arr.push(3);
+
 const arr1: number[] = [];
 const arr2: Array<number> = [1, 3];
 
@@ -9,3 +14,10 @@ arr3.push(['a', 'b'])
 type MyType = (string | number)
 const arr4: (string | number)[] = []
 //const arr4: MyType[] = []
+
+// кортежи (tuple)
+const tuple: [string, boolean, number] = ['abc', true, 0];
+// csv 
+type SimpleCsv = [string, string, number]
+const example: SimpleCsv[] = [['str', 'st', 32]];
+
