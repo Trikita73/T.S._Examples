f3f20dc5dbf2365999da5de6be3ac5f0e3a1136c
Trikita73 <diachenkoandrii7@gmail.com>
2024-11-04 15:28:40 +0200

    generic

--- generic.ts
+++ generic.ts
@@ -1,1 +1,44 @@
 // generic
+const valueFactory = (x: number) => x;
+const myValue = valueFactory(11);
+
+type TypeFactory<X> = X;
+type MyType = TypeFactory<string>;
+
+interface valueContainer<Value> {
+    value: Value;
+}
+
+type StringContainer = valueContainer<string>
+
+const x: StringContainer = {
+    value: 1, 
+}
+
+class ArrayOfNumbers {
+    constructor(public collection: number[]) {}
+
+    get(index: number): number {
+        return this.collection[index];
+    }
+}
+
+class ArrayOfString {
+    constructor(public collection: string[]) {}
+
+    get(index: number): string {
+        return this.collection[index];
+    }
+}
+
+class ArrayOfAnything<T> {
+    constructor(public collection: T[]) {}
+
+    get(index: number): T {
+        return this.collection[index];
+    }
+}
+
+new ArrayOfAnything<string>(['1', '2', 's']);
+new ArrayOfAnything<number>([0, 2, 3]);
+
